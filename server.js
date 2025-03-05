cc36e8e5df70fe6289438cf3c35c03c2314e5d34
vigimus <160164175+vigimus@users.noreply.github.com>
2025-03-05 23:17:40 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,98 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-paerser');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com'
+			password: 'cookies',
+			entries: 0, 
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'Sally',
+			email: 'sally@gmail.com'
+			password: 'bananas',
+			entries: 0, 
+			joined: new Date()
+		}
+	] 
+}
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+	if (req.body.email === database.users[0].email 
+		&& req.body.password ===database.users[0].password) {
+		res.json('sucess');
+	} else {
+		res.status(400).json('error logging in');
+	}
+})
+
+app.post('/register', (req, res) => {
+	const { email, name, password } = req.body;
+database.users.push({
+	id: '125',
+			name: name,
+			email: email,
+			password: password,
+			entries: 0, 
+			joined: new Date()
+})
+res.json(database.users[database.users.lenght-1]);
+})
+
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	const found = false; 
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		} 
+	})
+	if (!found) {
+		res.status(400).json('not found');
+	}
+})
+
+app.post('/image', (req, res) => {
+const { id } = req.body;
+	const found = false; 
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			user.entries++
+			return res.json(user.entries);
+		} 
+	})
+	if (!found) {
+		res.status(400).json('not found');
+	}
+})
+})
+
+app.listen(3000, ()=> {
+	console.log('App is running on port 3000');
+})
+
+/* 
+/ Svarar: This is working
+/signin så att man kan logga in --> POST (För att skydda lösenord)
+/register --> POST --> Användare
+/profile/userID --> GET --> Användare
+/image --> PUT --> Användare
+*/
